99725aa5809c5fb008ab58b3ee4e4602e1037bc0
Venkatesh Mankena <mankenavenkatesh@gmail.com>
2021-04-12 15:19:40 +0530

    Start Scanning from height at which bridge contract deployed
    when the chain starts from genesis state, as there are no events broadcasted, lowest_observed_nonce will be zero.
    
    Bridge relayer has to scan the events from the height at which bridge contract is deployed on ethereum.

--- orchestrator/oracle_resync.go
+++ orchestrator/oracle_resync.go
@@ -2,30 +2,16 @@ package orchestrator
 
 import (
 	"context"
-
-	"github.com/pkg/errors"
 )
 
 const defaultBlocksToSearch = 2000
 
 // GetLastCheckedBlock retrieves the last claim event this oracle has relayed to Cosmos.
 func (s *peggyOrchestrator) GetLastCheckedBlock(ctx context.Context) (uint64, error) {
-	latestHeader, err := s.ethProvider.HeaderByNumber(ctx, nil)
-	if err != nil {
-		err = errors.Wrap(err, "failed to get latest header")
-		return 0, err
-	}
-	currentBlock := latestHeader.Number.Uint64()
-
 	lastClaimEvent, err := s.cosmosQueryClient.LastClaimEventByAddr(ctx, s.peggyBroadcastClient.AccFromAddress())
 	if err != nil {
-		return currentBlock, err
-	} else if lastClaimEvent.EthereumEventNonce == 0 {
-		// TODO: Instead of currentBlock,return the block height at which peggy contract is deployed.
-		return currentBlock, nil
+		return uint64(0), err
 	}
 
 	return lastClaimEvent.EthereumEventHeight, nil
 }
-
-var ErrNotFound = errors.New("not found")
