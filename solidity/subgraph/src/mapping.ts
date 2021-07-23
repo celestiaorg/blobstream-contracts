c2cb763bbd559d6637c731f54d7d11fd2ddcbeb6
Markus Waas <mail@markuswaas.com>
2021-07-23 18:58:52 +0200

    update subgraph with additional data and transformations

--- solidity/subgraph/src/mapping.ts
+++ solidity/subgraph/src/mapping.ts
@@ -1,18 +1,136 @@
-import { BigInt, Bytes, log } from "@graphprotocol/graph-ts";
+// TODO not working in assembly script
+// import { bech32 } from "bech32";
+// import { Address as EthereumJSAddress } from "ethereumjs-util";
+
+import {
+  Address,
+  BigInt,
+  ByteArray,
+  Bytes,
+  crypto,
+  dataSource,
+  ethereum,
+  log,
+} from "@graphprotocol/graph-ts";
+
 import {
   ERC20DeployedEvent,
   SendToCosmosEvent,
   SubmitBatchCall,
-  UpdateValsetCall,
+  TransactionBatchExecutedEvent,
+  ValsetUpdatedEvent,
 } from "../generated/Peggy/Peggy";
 import {
   DeployedERC20,
   Deposit,
+  State,
+  StateLastBatchNonces,
   Valset,
+  BatchWithdrawal,
   Withdrawal,
 } from "../generated/schema";
 
+// setting peggy id directly as using call handler initialize was causing problems
+let STATE_PEGGY_ID_KOVAN = Bytes.fromHexString(
+  "0x696e6a6563746976652d70656767796964000000000000000000000000000000"
+);
+let STATE_PEGGY_ID_MAINNET = Bytes.fromHexString(
+  "0x696e6a6563746976652d70656767796964000000000000000000000000000000"
+);
+
+let STATE_STORE_ID = "1";
+
+function getInjectiveAddress(address: Bytes): string {
+  return address.toHexString();
+
+  // TODO not working in assembly script
+  // const addressBuffer = EthereumJSAddress.fromString(
+  //   address.toHexString()
+  // ).toBuffer();
+  // return bech32.encode("inj", bech32.toWords(addressBuffer));
+}
+
+function getCheckpoint(
+  peggyId: Bytes,
+  valsetNonce: BigInt,
+  validators: Address[],
+  powers: BigInt[],
+  rewardAmount: BigInt,
+  rewardToken: Address
+): ByteArray {
+  // bytes32 encoding of the string "checkpoint"
+  let methodName = Bytes.fromHexString(
+    "0x636865636b706f696e7400000000000000000000000000000000000000000000"
+  );
+
+  log.info("getCheckpoint: peggyId {} {} {} {}", [
+    peggyId.toString(),
+    valsetNonce.toHexString(),
+    rewardAmount.toHexString(),
+    rewardToken.toHexString(),
+  ]);
+  log.info("first validator {}", [validators[0].toHexString()]);
+  log.info("first power {}", [powers[0].toString()]);
+
+  let tupleArray: Array<ethereum.Value> = [
+    ethereum.Value.fromBytes(peggyId),
+    ethereum.Value.fromBytes(methodName as Bytes),
+    ethereum.Value.fromUnsignedBigInt(valsetNonce),
+    ethereum.Value.fromFixedBytesArray(validators as Bytes[]),
+    ethereum.Value.fromUnsignedBigIntArray(powers),
+    ethereum.Value.fromUnsignedBigInt(rewardAmount),
+    ethereum.Value.fromAddress(rewardToken),
+  ];
+
+  let encodedCheckpointData = ethereum.encode(
+    ethereum.Value.fromTuple(tupleArray as ethereum.Tuple)
+  )!;
+  let checkpoint = crypto.keccak256(encodedCheckpointData);
+
+  return checkpoint;
+}
+
 // TODO tracking the up-to-date state variables for convenience
+// state_lastEventNonce, state_lastValsetNonce, state_lastBatchNonces, state_lastValsetCheckpoint
+
+function updateState(
+  validators: Address[],
+  powers: BigInt[],
+  rewardAmount: BigInt,
+  rewardToken: Address,
+  lastEventNonce: BigInt,
+  lastValsetNonce: BigInt
+): void {
+  let state = new State(STATE_STORE_ID);
+  state.peggyId =
+    dataSource.network() == "mainnet"
+      ? (STATE_PEGGY_ID_MAINNET as Bytes)
+      : (STATE_PEGGY_ID_KOVAN as Bytes);
+
+  let checkpoint = getCheckpoint(
+    state.peggyId,
+    lastValsetNonce,
+    validators,
+    powers,
+    rewardAmount,
+    rewardToken
+  );
+
+  state.lastEventNonce = lastEventNonce.toI32();
+  state.lastValsetNonce = lastValsetNonce.toI32();
+  state.lastValsetCheckpoint = checkpoint as Bytes;
+
+  state.save();
+}
+
+function updateLastBatchNoncesState(
+  tokenAddress: Address,
+  lastBatchNonce: BigInt
+): void {
+  let stateLastBatchNonces = new StateLastBatchNonces(tokenAddress.toHex());
+  stateLastBatchNonces.nonce = lastBatchNonce.toI32();
+  stateLastBatchNonces.save();
+}
 
 export function handleNewERC20Deployed(event: ERC20DeployedEvent): void {
   log.info("handleNewERC20Deployed: {} at token with address {}", [
@@ -31,10 +149,14 @@ export function handleNewERC20Deployed(event: ERC20DeployedEvent): void {
   erc20.decimals = event.params._decimals;
 
   erc20.save();
+
+  let state = new State(STATE_STORE_ID);
+  state.lastEventNonce = event.params._eventNonce.toI32();
+  state.save();
 }
 
 export function handleSendToCosmosEvent(event: SendToCosmosEvent): void {
-  log.debug(
+  log.info(
     "handleSendToCosmosEvent: Withdrawal of {} at token with address {}",
     [event.params._amount.toString(), event.params._tokenContract.toHex()]
   );
@@ -45,42 +167,62 @@ export function handleSendToCosmosEvent(event: SendToCosmosEvent): void {
 
   deposit.tokenContract = event.params._tokenContract;
   deposit.amount = event.params._amount;
-  deposit.destination = event.params._destination;
+  deposit.destination = getInjectiveAddress(event.params._destination);
   deposit.sender = event.params._sender;
 
   deposit.save();
+
+  let state = new State(STATE_STORE_ID);
+  state.lastEventNonce = event.params._eventNonce.toI32();
+  state.save();
 }
 
-export function handleUpdateValset(call: UpdateValsetCall): void {
+export function handleUpdateValset(event: ValsetUpdatedEvent): void {
   log.info("handleUpdateValset: New valset length {} ", [
-    BigInt.fromI32(call.inputs._newValset.length).toString(),
+    BigInt.fromI32(event.params._validators.length).toString(),
   ]);
 
-  let valsetID = call.transaction.hash.toHex() + "-call";
+  let valsetID =
+    event.transaction.hash.toHex() + "-" + event.logIndex.toString();
   let valset = new Valset(valsetID);
 
-  valset.valsetNonce = call.inputs._currentValset.valsetNonce;
-  valset.rewardAmount = call.inputs._currentValset.rewardAmount;
-  valset.rewardToken = call.inputs._currentValset.rewardToken;
-  valset.powers = call.inputs._currentValset.powers;
+  valset.valsetNonce = event.params._newValsetNonce;
+  valset.rewardAmount = event.params._rewardAmount;
+  valset.rewardToken = event.params._rewardToken;
+  valset.powers = event.params._powers;
 
-  let addressTypeValidators = call.inputs._currentValset.validators;
-  let validators = new Array<Bytes>(
-    call.inputs._currentValset.validators.length
-  );
+  let addressTypeValidators = event.params._validators;
+  let validators = new Array<Bytes>(event.params._validators.length);
   for (let i = 0; i < addressTypeValidators.length; i++) {
     validators[i] = addressTypeValidators[i] as Bytes;
   }
   valset.validators = validators;
 
-  let bigIntTypePowers = call.inputs._currentValset.powers;
-  let powers = new Array<BigInt>(call.inputs._currentValset.powers.length);
+  let bigIntTypePowers = event.params._powers;
+  let powers = new Array<BigInt>(event.params._powers.length);
   for (let i = 0; i < bigIntTypePowers.length; i++) {
     powers[i] = bigIntTypePowers[i] as BigInt;
   }
   valset.powers = powers;
 
   valset.save();
+
+  updateState(
+    event.params._validators,
+    event.params._powers,
+    event.params._rewardAmount,
+    event.params._rewardToken,
+    event.params._eventNonce,
+    event.params._newValsetNonce
+  );
+}
+
+export function handleSubmitBatchEvent(
+  event: TransactionBatchExecutedEvent
+): void {
+  let state = new State(STATE_STORE_ID);
+  state.lastEventNonce = event.params._eventNonce.toI32();
+  state.save();
 }
 
 export function handleSubmitBatch(call: SubmitBatchCall): void {
@@ -89,20 +231,58 @@ export function handleSubmitBatch(call: SubmitBatchCall): void {
     [call.inputs._batchNonce.toString(), call.inputs._tokenContract.toHex()]
   );
 
-  let withdrawalID = call.transaction.hash.toHex() + "-call";
-  let withdrawal = new Withdrawal(withdrawalID);
+  let batchWithdrawalID =
+    call.transaction.hash.toHex() + "-" + call.inputs._batchNonce.toString();
+  let batchWithdrawal = new BatchWithdrawal(batchWithdrawalID);
 
-  withdrawal.amounts = call.inputs._amounts;
-  withdrawal.batchNonce = call.inputs._batchNonce;
-  withdrawal.fees = call.inputs._fees;
-  withdrawal.tokenContract = call.inputs._tokenContract;
+  batchWithdrawal.amounts = call.inputs._amounts;
+  batchWithdrawal.batchNonce = call.inputs._batchNonce;
+  batchWithdrawal.fees = call.inputs._fees;
+  batchWithdrawal.tokenContract = call.inputs._tokenContract;
 
   let addressTypeDestinations = call.inputs._destinations;
-  let destinations = new Array<Bytes>(call.inputs._destinations.length);
+  let destinations = new Array<string>(call.inputs._destinations.length);
   for (let i = 0; i < addressTypeDestinations.length; i++) {
-    destinations[i] = addressTypeDestinations[i] as Bytes;
+    destinations[i] = getInjectiveAddress(addressTypeDestinations[i] as Bytes);
   }
-  withdrawal.destinations = destinations;
+  batchWithdrawal.destinations = destinations;
+  batchWithdrawal.sender = call.from;
 
-  withdrawal.save();
+  let totalFee = BigInt.fromI32(0);
+  let fees = call.inputs._fees;
+
+  for (let i = 0; i < fees.length; i++) {
+    totalFee = totalFee.plus(fees[i]);
+  }
+  batchWithdrawal.totalFee = totalFee;
+
+  batchWithdrawal.save();
+
+  let amounts = call.inputs._amounts;
+
+  for (let i = 0; i < amounts.length; i++) {
+    let withdrawalID =
+      call.transaction.hash.toHex() +
+      "-" +
+      call.inputs._batchNonce.toString() +
+      "-" +
+      i.toString();
+    let withdrawal = new Withdrawal(withdrawalID);
+
+    withdrawal.amount = amounts[i];
+    withdrawal.destination = getInjectiveAddress(
+      addressTypeDestinations[i] as Bytes
+    );
+    withdrawal.fee = fees[i];
+    withdrawal.tokenContract = call.inputs._tokenContract;
+    withdrawal.timestamp = call.block.timestamp.toI32();
+    withdrawal.blockHeight = call.block.number.toI32();
+
+    withdrawal.save();
+  }
+
+  updateLastBatchNoncesState(
+    call.inputs._tokenContract,
+    call.inputs._batchNonce
+  );
 }
