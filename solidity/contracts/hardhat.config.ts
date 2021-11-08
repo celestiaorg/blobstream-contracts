1b56837bef9bdf90108dfb5951ba8991c4b5ceaf
John Adler <john.adler@protonmail.ch>
2021-11-08 10:46:20 -0500

    Hardhat and npm setup.

---
+++ solidity/contracts/hardhat.config.ts
@@ -0,0 +1,22 @@
+import { HardhatUserConfig } from 'hardhat/types';
+import '@nomiclabs/hardhat-waffle';
+import 'hardhat-deploy';
+import 'solidity-coverage';
+
+const config: HardhatUserConfig = {
+  defaultNetwork: 'hardhat',
+  solidity: {
+    compilers: [
+      {
+        version: '0.8.9',
+      },
+    ],
+  },
+  networks: {
+    coverage: {
+      url: 'http://127.0.0.1:8555',
+    },
+  },
+};
+
+export default config;
