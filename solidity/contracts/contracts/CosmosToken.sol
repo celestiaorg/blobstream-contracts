5921d21c41b2a7ad27acce55f40750ba81dd4622
John Adler <john.adler@protonmail.ch>
2021-11-08 11:04:40 -0500

    fmt

--- solidity/contracts/contracts/CosmosToken.sol
+++ solidity/contracts/contracts/CosmosToken.sol
@@ -5,20 +5,20 @@ pragma solidity ^0.8.0;
 import "./@openzeppelin/contracts/ERC20.sol";
 
 contract CosmosERC20 is ERC20 {
-	uint256 MAX_UINT = 2**256 - 1;
-	uint8 immutable private _decimals;
+    uint256 MAX_UINT = 2**256 - 1;
+    uint8 private immutable _decimals;
 
-	constructor(
-		address peggyAddress_,
-		string memory name_,
-		string memory symbol_,
-		uint8 decimals_
-	) ERC20(name_, symbol_) {
-		_decimals = decimals_;
-		_mint(peggyAddress_, MAX_UINT);
-	}
+    constructor(
+        address peggyAddress_,
+        string memory name_,
+        string memory symbol_,
+        uint8 decimals_
+    ) ERC20(name_, symbol_) {
+        _decimals = decimals_;
+        _mint(peggyAddress_, MAX_UINT);
+    }
 
-	function decimals() public view virtual override returns (uint8) {
-		return _decimals;
-	}
+    function decimals() public view virtual override returns (uint8) {
+        return _decimals;
+    }
 }
