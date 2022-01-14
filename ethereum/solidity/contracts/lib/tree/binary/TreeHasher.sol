bb136d2fe2d4f835394188da5a5aa08d87d43666
John Adler <john.adler@protonmail.ch>
2022-01-14 11:43:04 -0500

    Clean up.

--- ethereum/solidity/contracts/lib/tree/binary/TreeHasher.sol
+++ ethereum/solidity/contracts/lib/tree/binary/TreeHasher.sol
@@ -4,20 +4,13 @@ pragma experimental ABIEncoderV2;
 
 import "../Constants.sol";
 
-/// @notice Hash some data.
-/// @param data The data to be hashed.
-// solhint-disable-next-line func-visibility
-function hash(bytes memory data) pure returns (bytes32) {
-    return sha256(data);
-}
-
 /// @notice Calculate the digest of a node.
 /// @param left The left child.
 /// @param right The right child.
 /// @return digest The node digest.
 // solhint-disable-next-line func-visibility
 function nodeDigest(bytes32 left, bytes32 right) pure returns (bytes32 digest) {
-    digest = hash(abi.encodePacked(Constants.NODE_PREFIX, left, right));
+    digest = sha256(abi.encodePacked(Constants.NODE_PREFIX, left, right));
 }
 
 /// @notice Calculate the digest of a leaf.
@@ -25,5 +18,5 @@ function nodeDigest(bytes32 left, bytes32 right) pure returns (bytes32 digest) {
 /// @return digest The leaf digest.
 // solhint-disable-next-line func-visibility
 function leafDigest(bytes memory data) pure returns (bytes32 digest) {
-    digest = hash(abi.encodePacked(Constants.LEAF_PREFIX, data));
+    digest = sha256(abi.encodePacked(Constants.LEAF_PREFIX, data));
 }
