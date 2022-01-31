bceafac481cf3f08cf9b21838cb1bad21d45c459
John Adler <john.adler@protonmail.ch>
2022-01-30 21:50:57 -0500

    Use data root instead of message.

--- ethereum/solidity/contracts/IDAOracle.sol
+++ ethereum/solidity/contracts/IDAOracle.sol
@@ -1,19 +1,19 @@
 // SPDX-License-Identifier: Apache-2.0
 pragma solidity ^0.8.0;
 
-import "./MessageTuple.sol";
+import "./DataRootTuple.sol";
 import "./lib/tree/binary/BinaryMerkleProof.sol";
 
 /// @notice Data Availability Oracle interface.
 interface IDAOracle {
     /// @notice Verify a Data Availability attestation.
     /// @param _tupleRootIndex Index of the tuple root to prove against.
-    /// @param _tuple Message tuple to prove inclusion of.
+    /// @param _tuple Data root tuple to prove inclusion of.
     /// @param _proof Binary Merkle tree proof that `tuple` is in the root at `tupleRootIndex`.
     /// @return `true` is proof is valid, `false` otherwise.
     function verifyAttestation(
         uint256 _tupleRootIndex,
-        MessageTuple memory _tuple,
+        DataRootTuple memory _tuple,
         BinaryMerkleProof memory _proof
     ) external view returns (bool);
 }
