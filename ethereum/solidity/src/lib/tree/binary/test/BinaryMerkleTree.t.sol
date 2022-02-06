0f3ff4c455b5f16f9c6e07a624fedc35443f3d0c
John Adler <john.adler@protonmail.ch>
2022-02-06 13:27:14 -0500

    Add another test.

--- ethereum/solidity/src/lib/tree/binary/test/BinaryMerkleTree.t.sol
+++ ethereum/solidity/src/lib/tree/binary/test/BinaryMerkleTree.t.sol
@@ -10,6 +10,17 @@ contract BinaryMerkleProofTest is DSTest {
     function setUp() external {}
 
     function testVerifyNone() external {
+        bytes32 root = sha256("");
+        bytes32[] memory sideNodes;
+        uint256 key = 0;
+        uint256 numLeaves = 0;
+        BinaryMerkleProof memory proof = BinaryMerkleProof(sideNodes, key, numLeaves);
+        bytes memory data;
+        bool isValid = BinaryMerkleTree.verify(root, proof, data);
+        assertTrue(!isValid);
+    }
+
+    function testVerifyOneLeafEmpty() external {
         bytes32 root = 0x6e340b9cffb37a989ca544e6bb780a2c78901d3fb33738768511a30617afa01d;
         bytes32[] memory sideNodes;
         uint256 key = 0;
