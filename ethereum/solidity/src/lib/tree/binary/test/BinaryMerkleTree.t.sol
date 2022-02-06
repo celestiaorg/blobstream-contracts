8e0c7c6ef0c6e04c57f2b913e2b20f0be268374b
John Adler <john.adler@protonmail.ch>
2022-02-06 13:23:57 -0500

    Add test for binary tree.

---
+++ ethereum/solidity/src/lib/tree/binary/test/BinaryMerkleTree.t.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity ^0.8.0;
+
+import "ds-test/test.sol";
+
+import "../BinaryMerkleProof.sol";
+import "../BinaryMerkleTree.sol";
+
+contract BinaryMerkleProofTest is DSTest {
+    function setUp() external {}
+
+    function testVerifyNone() external {
+        bytes32 root = 0x6e340b9cffb37a989ca544e6bb780a2c78901d3fb33738768511a30617afa01d;
+        bytes32[] memory sideNodes;
+        uint256 key = 0;
+        uint256 numLeaves = 1;
+        BinaryMerkleProof memory proof = BinaryMerkleProof(sideNodes, key, numLeaves);
+        bytes memory data;
+        bool isValid = BinaryMerkleTree.verify(root, proof, data);
+        assertTrue(isValid);
+    }
+}
