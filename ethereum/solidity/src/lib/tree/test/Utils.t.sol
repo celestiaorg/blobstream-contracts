9d6e6fb00d602451c8f5a620627dfbba1bbd20e5
John Adler <john.adler@protonmail.ch>
2022-02-25 11:12:06 -0500

    Add tests for utils.

---
+++ ethereum/solidity/src/lib/tree/test/Utils.t.sol
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity ^0.8.0;
+
+import "ds-test/test.sol";
+
+import "../Utils.sol";
+
+contract UtilsTest is DSTest {
+    function testPathLengthFromKey0_8() external {
+        assertEq(pathLengthFromKey(0, 8), 3);
+    }
+
+    function testPathLengthFromKey1_8() external {
+        assertEq(pathLengthFromKey(1, 8), 3);
+    }
+
+    function testPathLengthFromKey2_8() external {
+        assertEq(pathLengthFromKey(2, 8), 3);
+    }
+
+    function testPathLengthFromKey3_8() external {
+        assertEq(pathLengthFromKey(3, 8), 3);
+    }
+
+    function testPathLengthFromKey4_8() external {
+        assertEq(pathLengthFromKey(4, 8), 3);
+    }
+
+    function testPathLengthFromKey5_8() external {
+        assertEq(pathLengthFromKey(5, 8), 3);
+    }
+
+    function testPathLengthFromKey6_8() external {
+        assertEq(pathLengthFromKey(6, 8), 3);
+    }
+
+    function testPathLengthFromKey7_8() external {
+        assertEq(pathLengthFromKey(7, 8), 3);
+    }
+}
