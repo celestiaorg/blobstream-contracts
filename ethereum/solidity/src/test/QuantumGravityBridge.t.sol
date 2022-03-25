44e0cf0e707a6d549df64ad5a5a1794434db856e
evan-forbes <evan.samuel.forbes@gmail.com>
2022-03-25 01:27:43 -0500

    spaces

--- ethereum/solidity/src/test/QuantumGravityBridge.t.sol
+++ ethereum/solidity/src/test/QuantumGravityBridge.t.sol
@@ -12,9 +12,9 @@ contract RelayerTest is DSTest {
 
     // private keys used for hardcoded signatures signatures
     // testAddr  = "0x9c2B12b5a07FC6D719Ed7646e5041A7E85758329"
-	// testPriv  = "64a1d6f0e760a8d62b4afdde4096f16f51b401eaaecc915740f71770ea76a8ad"
-	// testAddr2 = "0xe650B084f05C6194f6e552e3b9f08718Bc8a9d56"
-	// testPriv2 = "6e8bdfa979ab645b41c4d17cb1329b2a44684c82b61b1b060ea9b6e1c927a4f4"
+    // testPriv  = "64a1d6f0e760a8d62b4afdde4096f16f51b401eaaecc915740f71770ea76a8ad"
+    // testAddr2 = "0xe650B084f05C6194f6e552e3b9f08718Bc8a9d56"
+    // testPriv2 = "6e8bdfa979ab645b41c4d17cb1329b2a44684c82b61b1b060ea9b6e1c927a4f4"
 
     bytes32 constant VALIDATOR_SET_HASH_DOMAIN_SEPARATOR =
         0x636865636b706f696e7400000000000000000000000000000000000000000000;
