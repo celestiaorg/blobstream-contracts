210a1fbb9a9fc3165a038d8cedfba177c67a5726
John Adler <adlerjohn@users.noreply.github.com>
2022-06-06 13:59:59 -0400

    Universal nonce (#119)
    * Update contracts to use universal nonce.
    
    * Update tests.
    
    * Update Go wrapper.

--- src/IDAOracle.sol
+++ src/IDAOracle.sol
@@ -7,12 +7,12 @@ import "./lib/tree/binary/BinaryMerkleProof.sol";
 /// @notice Data Availability Oracle interface.
 interface IDAOracle {
     /// @notice Verify a Data Availability attestation.
-    /// @param _tupleRootIndex Index of the tuple root to prove against.
+    /// @param _tupleRootNonce Nonce of the tuple root to prove against.
     /// @param _tuple Data root tuple to prove inclusion of.
-    /// @param _proof Binary Merkle tree proof that `tuple` is in the root at `tupleRootIndex`.
+    /// @param _proof Binary Merkle tree proof that `tuple` is in the root at `_tupleRootNonce`.
     /// @return `true` is proof is valid, `false` otherwise.
     function verifyAttestation(
-        uint256 _tupleRootIndex,
+        uint256 _tupleRootNonce,
         DataRootTuple memory _tuple,
         BinaryMerkleProof memory _proof
     ) external view returns (bool);
