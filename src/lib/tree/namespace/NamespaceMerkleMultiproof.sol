aad55f711704d1bcd9ebd905d7166fc201ec1cff
John Adler <john.adler@protonmail.ch>
2022-11-27 18:21:16 -0500

    Remove unused leaf field.

--- src/lib/tree/namespace/NamespaceMerkleMultiproof.sol
+++ src/lib/tree/namespace/NamespaceMerkleMultiproof.sol
@@ -11,10 +11,6 @@ struct NamespaceMerkleMultiproof {
     uint256 endKey;
     // List of side nodes to verify and calculate tree.
     NamespaceNode[] sideNodes;
-    // Empty if the namespace is present in the tree. In case the namespace to
-    // be proved is in the min/max range of the tree but absent, this will
-    // contain the leaf node necessary to verify the proof of absence.
-    NamespaceNode[] leafNode;
     // The number of leaves in the tree.
     uint256 numLeaves;
 }
