fda61aff1aeff1233e968cd64b644eabf0c2b3c2
rachid <chamirachid1@gmail.com>
2023-03-24 10:02:16 +0100

    chore: generate the wrappers from .abi and .bin

---
+++ scripts/gen.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+set -e
+
+forge build > /dev/null
+
+cd "${SOLIDITY_SRC_DIR}"
+
+for file in ${CONTRACTS}
+do
+    mkdir -p ../wrappers/"${file}"
+    contractName=$(echo "${file}" | cut -d . -f 1)
+
+    jq .abi < ../out/"${file}"/"${contractName}".json > ../out/"${file}"/"${contractName}".abi
+    jq .bytecode.object < ../out/"${file}"/"${contractName}".json | cut -d \" -f 2 > ../out/"${file}"/"${contractName}".bin
+
+    abigen --type=qgb --pkg wrappers \
+        --out=../wrappers/"${file}"/wrapper.go \
+        --abi ../out/"${file}"/"${contractName}".abi \
+        --bin ../out/"${file}"/"${contractName}".bin
+done
+
+echo "done."
