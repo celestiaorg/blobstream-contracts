313e36afddc6ec2fa50137524ca29b6bb7aea0e5
CHAMI Rachid <chamirachid1@gmail.com>
2023-05-01 14:26:30 +0200

    Update src/DataRootTuple.sol

--- src/DataRootTuple.sol
+++ src/DataRootTuple.sol
@@ -8,6 +8,7 @@ pragma solidity ^0.8.4;
 struct DataRootTuple {
     // Celestia block height the data root was included in.
     // Genesis block is height = 0.
+    // First queryable block is height = 1.
     uint256 height;
     // Data root.
     bytes32 dataRoot;
