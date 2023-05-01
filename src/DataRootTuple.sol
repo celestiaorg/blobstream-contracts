03f0ab6f6eceba7a7c74a46b2c2a1fd1e390010c
CHAMI Rachid <chamirachid1@gmail.com>
2023-05-01 23:42:18 +0200

    chore: update data root tuple genesis comment (#144)
    <!--
    Please read and fill out this form before submitting your PR.
    
    Please make sure you have reviewed our contributors guide before
    submitting your
    first PR.
    -->
    
    ## Overview
    
    <!-- 
    Please provide an explanation of the PR, including the appropriate
    context,
    background, goal, and rationale. If there is an issue with this
    information,
    please provide a tl;dr and link the issue. 
    -->
    
    ## Checklist
    
    <!-- 
    Please complete the checklist to ensure that the PR is ready to be
    reviewed.
    
    IMPORTANT:
    PRs should be left in Draft until the below checklist is completed.
    -->
    
    - [ ] New and updated code has appropriate documentation
    - [ ] New and updated code has new and/or updated testing
    - [ ] Required CI checks are passing
    - [ ] Visual proof for any user facing features like CLI or
    documentation updates
    - [ ] Linked issues closed with keywords

--- src/DataRootTuple.sol
+++ src/DataRootTuple.sol
@@ -7,7 +7,8 @@ pragma solidity ^0.8.4;
 ///  https://github.com/celestiaorg/celestia-specs/blob/master/src/specs/data_structures.md#header
 struct DataRootTuple {
     // Celestia block height the data root was included in.
-    // Genesis block is height = 1.
+    // Genesis block is height = 0.
+    // First queryable block is height = 1.
     uint256 height;
     // Data root.
     bytes32 dataRoot;
