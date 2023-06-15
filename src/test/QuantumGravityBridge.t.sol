ab3a312c1c14d60eb85ca1dd6e680fcc2e98e1e6
John Adler <adlerjohn@users.noreply.github.com>
2023-06-15 09:40:07 -0400

    Remove redundant ECDSA OpenZeppelin library. (#151)
    The `ECDSA.sol` library already exists in `lib/openzeppelin-contracts`.

--- src/test/QuantumGravityBridge.t.sol
+++ src/test/QuantumGravityBridge.t.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: Apache-2.0
 pragma solidity ^0.8.19;
 
-import "../lib/openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+import "openzeppelin-contracts/contracts/utils/cryptography/ECDSA.sol";
 
 import "../Constants.sol";
 import "../DataRootTuple.sol";
