3070e7a427cb489c11fec03c7c2b11bc8d37eca0
CHAMI Rachid <chamirachid1@gmail.com>
2023-08-03 10:02:33 +0200

    feat: support compute square size from proofs (#164)
    <!--
    Please read and fill out this form before submitting your PR.
    
    Please make sure you have reviewed our contributors guide before
    submitting your
    first PR.
    -->
    
    ## Overview
    
    <!-- 
    Please provide an explanation of the PR, including the appropriate
    context,
    background, goal, and rationale. If there is an issue with this
    information,
    please provide a tl;dr and link the issue. 
    -->
    
    Closes https://github.com/celestiaorg/quantum-gravity-bridge/issues/161
    
    I will open a subsequent PR to provide ways to authenticate the proofs.
    
    ## Checklist
    
    <!-- 
    Please complete the checklist to ensure that the PR is ready to be
    reviewed.
    
    IMPORTANT:
    PRs should be left in Draft until the below checklist is completed.
    -->
    
    - [ ] New and updated code has appropriate documentation
    - [ ] New and updated code has new and/or updated testing
    - [ ] Required CI checks are passing
    - [ ] Visual proof for any user facing features like CLI or
    documentation updates
    - [ ] Linked issues closed with keywords

--- src/lib/verifier/test/DAVerifier.t.sol
+++ src/lib/verifier/test/DAVerifier.t.sol
@@ -186,6 +186,77 @@ contract DAVerifierTest is DSTest {
         assertTrue(valid);
     }
 
+    function testComputeSquareSizeFromRowProof() public {
+        BinaryMerkleProof memory _rowProof;
+        _rowProof.key = 0;
+        _rowProof.numLeaves = 4;
+        bytes32[] memory _sideNodes = new bytes32[](2);
+        _sideNodes[0] = 0x3d9568eda3d860f78af0b605066eb6f90495b87d2081bf875e6a88ede0a1f6bc;
+        _sideNodes[1] = 0xbf6d9e948bc2e4bc32a791135385bbc2a3b8f426d33d212c9f755e8f2dd964ad;
+        _rowProof.sideNodes = _sideNodes;
+
+        bytes memory _rowRoot = abi.encodePacked(
+            hex"000000000000000100000000000000018c8732952e0c3e3f0adf0a43665e30bc554cfad53635caccb52c7d38cc078af8"
+        );
+        bytes32 _root = 0x1108C0D8079563116167A66BE596DBE222E438C273ECC3B48E290465FC6093B2;
+
+        // check that the merkle proof is valid
+        bool validMerkleProof = BinaryMerkleTree.verify(_root, _rowProof, _rowRoot);
+        assertTrue(validMerkleProof);
+
+        // check that the computed square size is correct
+        uint256 expectedSquareSize = 1;
+        uint256 actualSquareSize = DAVerifier.computeSquareSizeFromRowProof(_rowProof);
+        assertEq(actualSquareSize, expectedSquareSize);
+    }
+
+    function testComputeSquareSizeFromShareProof() public {
+        bytes[] memory _data = new bytes[](1);
+        _data[0] = abi.encodePacked(
+            hex"0000000000000001010000014300000011c1020a95010a92010a1c2f636f736d",
+            hex"6f732e62616e6b2e763162657461312e4d736753656e6412720a2f63656c6573",
+            hex"7469613165383064747a75387a38786739676d7333716d346c34336639757a6c",
+            hex"306174767473766a3564122f63656c65737469613167616b61646d63386a7366",
+            hex"7873646c676e6d64643867773736346739796165776e32726d386d1a0e0a0475",
+            hex"746961120631303030303012650a500a460a1f2f636f736d6f732e6372797074",
+            hex"6f2e736563703235366b312e5075624b657912230a2102207a8037a3a1dac112",
+            hex"f77d982feaca3d8930e468b835a11ff176a159588334f312040a020801180112",
+            hex"110a0b0a0475746961120335303010d0e80c1a40c19753445b3de4d70d6c2570",
+            hex"7d082968e8fd8c8b8fb4e135a570c8d291e90a7b30219bf5ab4840081c1479d8",
+            hex"295a5d73ef1d635faf40467bbe7658398d24f1d6000000000000000000000000",
+            hex"0000000000000000000000000000000000000000000000000000000000000000",
+            hex"0000000000000000000000000000000000000000000000000000000000000000",
+            hex"0000000000000000000000000000000000000000000000000000000000000000",
+            hex"0000000000000000000000000000000000000000000000000000000000000000",
+            hex"0000000000000000000000000000000000000000000000000000000000000000"
+        );
+
+        NamespaceMerkleMultiproof memory _shareProof;
+        _shareProof.beginKey = 0;
+        _shareProof.endKey = 1;
+        NamespaceNode[] memory sideNodes = new NamespaceNode[](1);
+        sideNodes[0].min = NamespaceID.wrap(0xffffffffffffffff);
+        sideNodes[0].max = NamespaceID.wrap(0xffffffffffffffff);
+        sideNodes[0].digest = 0x99ff60ce3818df2d1601a5a6a7d7bac82aa79d1726bca4e05b94e4ce38f06ffe;
+        _shareProof.sideNodes = sideNodes;
+
+        NamespaceID _minimaxNID = NamespaceID.wrap(0x0000000000000001);
+
+        NamespaceNode memory _rowRoot;
+        _rowRoot.min = NamespaceID.wrap(0x0000000000000001);
+        _rowRoot.max = NamespaceID.wrap(0x0000000000000001);
+        _rowRoot.digest = 0x8C8732952E0C3E3F0ADF0A43665E30BC554CFAD53635CACCB52C7D38CC078AF8;
+
+        // check that the merkle proof is valid
+        bool validMerkleProof = NamespaceMerkleTree.verifyMulti(_rowRoot, _shareProof, _minimaxNID, _data);
+        assertTrue(validMerkleProof);
+
+        // check that the computed square size is correct
+        uint256 expectedSquareSize = 1;
+        uint256 actualSquareSize = DAVerifier.computeSquareSizeFromShareProof(_shareProof);
+        assertEq(actualSquareSize, expectedSquareSize);
+    }
+
     function computeValidatorSetHash(Validator[] memory _validators) private pure returns (bytes32) {
         return keccak256(abi.encode(_validators));
     }
