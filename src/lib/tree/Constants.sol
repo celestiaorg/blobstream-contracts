354aadf24a74f855f6a3d4b9062e4c8cfa81884c
rachid <chamirachid1@gmail.com>
2023-09-07 13:18:30 +0200

    chore: remove unnecessary constant

--- src/lib/tree/Constants.sol
+++ src/lib/tree/Constants.sol
@@ -14,10 +14,6 @@ library Constants {
     /// @dev The prefixes of leaves and nodes
     bytes1 internal constant LEAF_PREFIX = 0x00;
     bytes1 internal constant NODE_PREFIX = 0x01;
-
-    /// @dev Parity share namespace in bytes.
-    bytes29 internal constant PARITY_SHARE_NAMESPACE_BYTES =
-        0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF;
 }
 
 /// @dev Parity share namespace.
