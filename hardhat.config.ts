cf301adfbfdae138526199fab805822400dcfd5d
CHAMI Rachid <chamirachid1@gmail.com>
2023-10-03 15:30:17 +0200

    feat: add support for hardhat project (#214)
    <!--
    Please read and fill out this form before submitting your PR.
    
    Please make sure you have reviewed our contributors guide before
    submitting your
    first PR.
    -->
    
    ## Overview
    
    This will lay foundations for using deployment and upgrades via the uups
    plugin, run static code analyzers, etc.
    
    Let me know what you think of having 3 projects in the same repo:
    - go project
    - foundry project
    - hardhat project
    
    ## Checklist
    
    <!-- 
    Please complete the checklist to ensure that the PR is ready to be
    reviewed.
    
    IMPORTANT:
    PRs should be left in Draft until the below checklist is completed.
    -->
    
    - [ ] New and updated code has appropriate documentation
    - [ ] New and updated code has new and/or updated testing
    - [ ] Required CI checks are passing
    - [ ] Visual proof for any user facing features like CLI or
    documentation updates
    - [ ] Linked issues closed with keywords

---
+++ hardhat.config.ts
@@ -0,0 +1,46 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+import "hardhat-preprocessor";
+import fs from "fs";
+
+const config: HardhatUserConfig = {
+  solidity: {
+    version: "0.8.20",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+      viaIR: true
+    },
+  },
+  preprocess: {
+    eachLine: (hre) => ({
+      transform: (line: string) => {
+        if (line.match(/^\s*import /i)) {
+          for (const [from, to] of getRemappings()) {
+            if (line.includes(from)) {
+              line = line.replace(from, to);
+              break;
+            }
+          }
+        }
+        return line;
+      },
+    }),
+  },
+  paths: {
+    sources: "./src",
+    cache: "./cache_hardhat",
+  },
+};
+
+export default config;
+
+function getRemappings() {
+  return fs
+      .readFileSync("remappings.txt", "utf8")
+      .split("\n")
+      .filter(Boolean) // remove empty lines
+      .map((line) => line.trim().split("="));
+}
---
+++ scripts/deploy.ts
@@ -0,0 +1,12 @@
+import { ethers } from "hardhat";
+
+async function main() {
+  // TODO add deploy script
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
