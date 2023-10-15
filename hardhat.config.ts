3dc8592385725c844f1f4890410b01ad5f595bc3
CHAMI Rachid <chamirachid1@gmail.com>
2023-10-16 01:59:16 +0200

    chore(deps): bump solidity version to v0.8.21 (#221)
    <!--
    Please read and fill out this form before submitting your PR.
    
    Please make sure you have reviewed our contributors guide before
    submitting your
    first PR.
    -->
    
    ## Overview
    
    When verifying the QGB contract, these were signaled by Etherscan:
    
    https://sepolia.etherscan.io/solcbuginfo?a=FullInlinerNonExpressionSplitArgumentEvaluationOrder
    
    https://sepolia.etherscan.io/solcbuginfo?a=MissingSideEffectsOnSelectorAccess
    
    Which are fixed with this version bump.
    
    Also closes:
    https://github.com/celestiaorg/quantum-gravity-bridge/issues/136
    
    ## Checklist
    
    <!-- 
    Please complete the checklist to ensure that the PR is ready to be
    reviewed.
    
    IMPORTANT:
    PRs should be left in Draft until the below checklist is completed.
    -->
    
    - [ ] New and updated code has appropriate documentation
    - [ ] New and updated code has new and/or updated testing
    - [ ] Required CI checks are passing
    - [ ] Visual proof for any user facing features like CLI or
    documentation updates
    - [ ] Linked issues closed with keywords

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -5,7 +5,7 @@ import fs from "fs";
 
 const config: HardhatUserConfig = {
   solidity: {
-    version: "0.8.20",
+    version: "0.8.21",
     settings: {
       optimizer: {
         enabled: true,
--- src/lib/tree/binary/test/BinaryMerkleTree.t.sol
+++ src/lib/tree/binary/test/BinaryMerkleTree.t.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: Apache-2.0
-pragma solidity ^0.8.19;
+pragma solidity ^0.8.21;
 
 import "ds-test/test.sol";
 
